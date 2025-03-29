79956bdc0d4922bea71d4b75852e1638c00e5c47
Bismark Arroyo <bis.arroyo@gmail.com>
2025-03-29 13:35:27 -0600

    add more text for display in the game

--- src/data.js
+++ src/data.js
@@ -1,1 +1,2 @@
-export const text1 = 'lorem Imsum'
+export const text1 =
+  'The quick brown fox jumps over the lazy dog. Bright stars shine above the silent town. Waves crash upon the sandy shore. A secret lies hidden beneath the old oak tree. Time moves fast, yet memories remain. Dreams take flight when courage leads the way. Keep typing, never stop learning!'
