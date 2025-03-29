d1b8e8f999abe40a0bf3709f5df6ed699800a003
Bismark Arroyo <bis.arroyo@gmail.com>
2025-03-29 13:36:23 -0600

    add logic for game

--- src/main.js
+++ src/main.js
@@ -5,20 +5,26 @@ import viteLogo from '/vite.svg'
 import { text1 } from './data.js'
 
 const $ = (element) => document.querySelector(element)
-const $$ = (element) => document.querySelector(element)
+const $$ = (element) => document.querySelectorAll(element)
 
 // results section
 const results = $('#results')
+const wpmResult = $('#wpm-result')
+const timeenlapsed = $('#time-enlapsed')
 
 // game section
 const game = $('#game')
 const timer = $('#timer')
 const paragraph = $('#letters')
 const input = $('#input')
+const wpm = $('#wpm')
 
-const INITIAL_TIME = 30
+// Constants
+const INITIAL_TIME = 0
 
-let words = text1.split('')
+let letters = text1.split('')
+let words = text1.split(' ')
+let wordsAverage = parseInt(letters.length) / parseInt(words.length)
 let currentTime = INITIAL_TIME
 let isPlaying
 
@@ -31,8 +37,8 @@ function initGame() {
 
   isPlaying = false
 
-  timer.textContent = 30
-  let gameWords = words.map((letter, index) => {
+  timer.textContent = INITIAL_TIME
+  let gameWords = letters.map((letter, index) => {
     return `<span class="letter">${letter}</span>`
   })
   paragraph.innerHTML = gameWords.join('')
@@ -52,45 +58,86 @@ function startPlaying() {
   if (!isPlaying) {
     isPlaying = true
     const gameInterval = setInterval(() => {
-      currentTime--
+      currentTime++
       timer.textContent = currentTime
-      if (currentTime == 0) {
-        clearInterval(gameInterval)
-        gameOver()
-      }
+      // if (currentTime == 0) {
+      //   clearInterval(gameInterval)
+      //   gameOver()
+      // }
     }, 1000)
+    const wpmCalc = setInterval(() => {
+      wpm.innerText = calculateWordsPerMinute()
+      if (!isPlaying) {
+        clearInterval(wpmCalc)
+      }
+    }, 5000)
   }
 }
 
+function goPrevWord(activeElement) {
+  let prevWord = activeElement.previousElementSibling
+  // validate that is a word before the active element
+  if (prevWord !== null) {
+    prevWord.classList = 'letter active'
+    activeElement.classList = 'letter'
+  }
+}
+function goNextWord(activeElement) {
+  let nextWord = activeElement.nextElementSibling
+  // validate that is a word after the active element
+  if (nextWord === null) {
+    // if completes the las word the game will end
+    gameOver()
+  } else {
+    nextWord.classList.add('active')
+    activeElement.classList.remove('active')
+  }
+}
 function onkeyDown(event) {
   const active = $('span.active')
 
   let { key } = event
-
   if (key === 'Backspace') {
+    goPrevWord(active)
   }
 }
 
 function onkeyUp(event) {
   let active = $('span.active')
   let activeContent = active.innerText.trim()
-  console.log(activeContent)
+
   let { key } = event
 
-  if (key === activeContent) {
+  let validkey = key.length === 1 ? activeContent : 'invalid'
+  if (validkey === 'invalid') {
+    return
+  }
+
+  if (key === validkey) {
     active.classList.add('correct')
+    goNextWord(active)
   } else {
     active.classList.add('incorrect')
+    goNextWord(active)
   }
-  let nextWord = active.nextElementSibling
-  nextWord.classList.add('active')
-  active.classList.remove('active')
 }
 
 function gameOver() {
+  isPlaying = false
   game.style.display = 'none'
   results.style.display = 'flex'
-  currentTime = 0
 
-  const correctLetters = $$('span.correct')
+  wpmResult.innerText = calculateWordsPerMinute()
+  timeenlapsed.innerText = currentTime
+}
+gameOver()
+
+function calculateWordsPerMinute() {
+  let correctWords = $$('.correct').length / wordsAverage
+  console.log(correctWords)
+  if (correctWords) {
+    let result = Math.floor(correctWords / (currentTime / 60))
+    return result
+  }
+  return 0
 }
